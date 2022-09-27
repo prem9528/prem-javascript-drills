246f9e46f0aa5be934301e06c3b9b18731fabc1a
prem <sonanios2010@gmail.com>
2022-09-27 17:19:46 +0530

    commiting solution for javascript drills.

--- array-methods-impl.js
+++ array-methods-impl.js
@@ -36,43 +36,75 @@ map(items, cb1)
 let sum = 0;
 function cb2(a, b) {
   sum += b;
-   console.log(sum+a);
+  console.log(sum + a);
 }
 
 function reduce(elements, cb, startingValue) {
-  console.log(startingValue)
   for (let i of elements) {
-    if (startingValue == 'undefined') {
-      console.log(startingValue+"gjhgjhv")
-      startingValue = elements[0];
+    if (typeof (startingValue) == 'undefined') {
+      startingValue = 0;
       cb(0, i)
     } else {
       cb(startingValue, i)
     }
-  }  
+  }
 }
-reduce(items, cb2)
+reduce(items, cb2, 10)
+// console.log('when startingValue is undefined ')
+// reduce(items, cb2)
 
 //<-------------------------------------------------------------------------------------------------->
+function cb3(a) {
+  if (!a) {
+    console.log(`undefined`);
+  } else {
+    console.log(a);
+  }
+}
+
 function find(elements, cb) {
-  // Do NOT use .includes, to complete this function.
-  // Look through each value in `elements` and pass each element to `cb`.
-  // If `cb` returns `true` then return that element.
-  // Return `undefined` if no elements pass the truth test.
+  for (let i of elements) {
+    cb(i)
+  }
 }
+find(items, cb3)
 
 //<-------------------------------------------------------------------------------------------------->
+let arr = [];
+function cb4(a) {
+  if (!a) {
+    console.log([]);
+  } else {
+    arr.push(a);
+    console.log(arr);
+  }
+}
+
 function filter(elements, cb) {
+  for (let i of elements) {
+    cb(i)
+  }
   // Do NOT use .filter, to complete this function.
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
 }
+filter(items, cb4)
 
 //<-------------------------------------------------------------------------------------------------->
 const nestedArray = [1, [2], [[3]], [[[4]]]]; // use this to test 'flatten'
 
 function flatten(elements) {
-  // Flattens a nested array (the nesting can be to any depth).
-  // Hint: You can solve this using recursion.
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  let result = []
+  for (let i of elements) {
+    if (Array.isArray(i)) {
+      result = result.concat(flatten(i))
+    }
+    else {
+      result.push(i)
+    }
+  }
+  return result
+
 }
+let xy = flatten(nestedArray);
+console.log(xy)
