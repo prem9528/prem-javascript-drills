d664f9e4e3d79f06d7daef96bd40d8d56684b668
prem <sonanios2010@gmail.com>
2022-09-28 15:23:48 +0530

    commiting solutions

--- closure.js
+++ closure.js
@@ -8,9 +8,6 @@ function counterFactory() {
 //<----------------------------------------------------------------------------------------------->
 
 function limitFunctionCallCount(cb, n) {
-  // Should return a function that invokes `cb`.
-  // The returned function should only allow `cb` to be invoked `n` times.
-  // Returning null is acceptable if cb can't be returned
   let callCount = 0;
   return (...args) => {
     if (callCount === n) return null;
@@ -24,12 +21,7 @@ function limitFunctionCallCount(cb, n) {
 
 
 function cacheFunction(cb) {
-  // Should return a funciton that invokes `cb`.
-  // A cache (object) should be kept in closure scope.
-  // The cache should keep track of all arguments have been used to invoke this function.
-  // If the returned function is invoked with arguments that it has already seen
-  // then it should return the cached result and not invoke `cb` again.
-  // `cb` should only ever be invoked once for a given set of arguments.
+ 
   const cache = {};
   return (input) => {
     if (Object.prototype.hasOwnProperty.call(cache, input)) return cache[input];
