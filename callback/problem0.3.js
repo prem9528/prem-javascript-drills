3648d98cbef1eaa73b90a6603bc39fd5470c6450
prem <sonanios2010@gmail.com>
2022-10-06 13:08:28 +0530

    committing solution for q01

--- callback/problem0.3.js
+++ callback/problem0.3.js
@@ -1,28 +1,28 @@
 const fs = require("fs");
 
-fs.readFile("./sample1.json","utf-8",(err, data)=>{
-if(err){
-    console.log(err);
-}else{
-    
-    fs.appendFile(`./content/renderedSecondFile.json`, data, "utf-8", (err)=>{
-        if(err){
-            console.log(err)
-        }else{
-            fs.unlink("./sample1.json", (err)=>{
-                if(err){
-                    console.log(err)
-                }else{
-                    fs.unlink(`./content/renderedSecondFile.json`, (err)=>{
-                        if(err){
-                            console.log(err);
-                        }else{
-                            console.log("task done");
-                        }
-                    })
-                }
-            })
-        }
-    })
-}
+fs.readFile("./sample1.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+
+        fs.appendFile(`./content/renderedSecondFile.json`, data, "utf-8", (err) => {
+            if (err) {
+                console.log(err)
+            } else {
+                fs.unlink("./sample1.json", (err) => {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        fs.unlink(`./content/renderedSecondFile.json`, (err) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log("task done");
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    }
 });
---
+++ callback/problem1.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+
+fs.mkdir("./test02", (err) => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log('Directory created successfully!');
+    let a = Math.round(Math.random() * 10);
+    let data = require("./sample1.json");
+    fs.appendFile(`./test02/rendered${a}.json`, JSON.stringify(data), "utf-8", (err) => {
+        if (err) {
+            console.log(err)
+        }else{
+            fs.readdir("./test02", (err, files)=>{
+                if(err){
+                    console.log(err)
+                }else{
+                    // console.log(files);
+                    
+                    files.forEach((a)=>{
+                        console.log(JSON.stringify(files));
+                        fs.unlink(`./test02/${a}`, (err)=>{
+                            if(err){
+                                console.log(err)
+                            }else{
+                                console.log(`files deleted  ${files.length}`);
+                            }
+            
+                        })}
+                    )
+                
+                }
+            })
+        }
+    })
+});
