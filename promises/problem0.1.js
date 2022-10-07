effc135c07d3fcf9f08748a8910dadc328c0efe5
prem <sonanios2010@gmail.com>
2022-10-07 09:56:58 +0530

    committing changes with promise problem 0.1

---
+++ promises/problem0.1.js
@@ -0,0 +1,12 @@
+const fs = require("fs").promises;
+
+const randomFile = fs.readFile(`./sample1.json`, "utf-8");
+
+randomFile.then((data)=> {
+    let a = Math.round(Math.random() * 10);
+    const rander= fs.appendFile(`./content/rander${a}.json`, data, "utf-8")
+    
+    return rander
+}).catch((err)=>{
+console.log(err);
+})
